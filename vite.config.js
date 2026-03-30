4ef75ec846b76af3d06a36d39827fa80c471d29d
anshdhariwal <anshdhariwalhsr@gmail.com>
2026-03-30 10:47:42 +0530

    configured for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/calci/',
   server: { port: 6969 },
-
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'terser',
+  },
 })
